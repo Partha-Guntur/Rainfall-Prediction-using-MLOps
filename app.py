79efa01a6a7b47515aeb648b10012cad482562e2
partha <parthagunturu2003@gmail.com>
2024-12-29 23:37:21 +0530

    Commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Rainfall Prediction Webpage"
+    return "Rainfall Prediction"
 
 if __name__ == "__main__":
     app.run(debug=True)
